704c0d1920b607af5b2dd6f57f97bdb660850ad3
zroyer <zroyer@utexas.edu>
2018-07-20 14:49:58 -0500

    adds basic square component

---
+++ src/components/Square.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+  renderSquare() {
+    const {
+      isMine,
+      isFlag,
+      isSearched,
+      squareScore
+    } = this.props
+
+    if (isFlag) {
+      return '🚩'
+    }
+    if (isMine) {
+      return '💣'
+    }
+
+    return squareScore
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderSquare()}
+      </div>
+    );
+  }
+}
+
+export default Square;
